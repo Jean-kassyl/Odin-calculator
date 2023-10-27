cb3a1580cd5ffab85b9bad812d740bc820fb03ca
Jean Kassyl <kassylmac@gmail.com>
2023-10-27 07:39:52 +0200

    work on the design of the app (styles)

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log("Welcome to the calculator app")
+const btns = document.querySelectorAll('.btn');
+const operation = document.querySelector('.operation')
+const result = document.querySelector('.result')
+
+
+
+btns.forEach(btn => {
+    btn.addEventListener("click", (e) => {
+        let op = ""
+        op = e.currentTarget.textContent 
+        operation.textContent += op + " " 
+    })
+})
